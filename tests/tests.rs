3009306f66947fec84c8d6910fab07097b63f96d
Leonid Meleshin <hello@leon0399.ru>
2023-10-09 02:26:10 +0400

    feat: support specifying times to run

--- tests/tests.rs
+++ tests/tests.rs
@@ -16,8 +16,38 @@ fn fail_first_two_times() {
   assert_eq!(3, C.load(Ordering::SeqCst));
 }
 
+#[flaky_test(5)]
+fn fail_first_four_times() {
+  static C: AtomicUsize = AtomicUsize::new(0);
+  if C.fetch_add(1, Ordering::SeqCst) < 4 {
+    panic!("flaky");
+  }
+  assert_eq!(5, C.load(Ordering::SeqCst));
+}
+
+#[flaky_test(times = 10)]
+fn fail_first_nine_times() {
+  static C: AtomicUsize = AtomicUsize::new(0);
+  if C.fetch_add(1, Ordering::SeqCst) < 9 {
+    panic!("flaky");
+  }
+  assert_eq!(10, C.load(Ordering::SeqCst));
+}
+
 #[flaky_test]
 #[should_panic]
 fn fail_three_times() {
+  assert!(false);
+}
+
+#[flaky_test(5)]
+#[should_panic]
+fn fail_five_times() {
+  assert!(false);
+}
+
+#[flaky_test(times = 10)]
+#[should_panic]
+fn fail_ten_times() {
   assert!(false);
 }
